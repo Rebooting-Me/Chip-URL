bdf7d66c21b29c44b003a1fa1d207e8ac6b86fcd
Rebooting-Me <arsharma@ucsd.edu>
2023-05-30 17:52:54 -0700

    added and styled the modal

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -74,13 +74,50 @@ const Form = ({ isAuthenticated }) => {
         </button>
       </form>
       {showOverlay && (
-          <div className="overlay">
-            <h3>Short URL Generated!</h3>
+        <div className="overlay">
+          <div
+            className="overlay-content"
+            style={{
+              position: "absolute",
+              zIndex: "10000",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "25rem",
+              height: "fit-content",
+              padding: "5px 20px",
+              backgroundColor: "#1C1B22",
+            }}
+          >
+            <h2>Short URL Generated!</h2>
             <CopyToClipboard
               text={generatedUrl}
               onCopy={() => setIsCopied(true)}
             >
-              <p>{generatedUrl}</p>
+              <div
+                style={{
+                  width: 300,
+                  height: 65,
+                  borderRadius: 20,
+                  borderStyle: "solid",
+                  borderWidth: 3.5,
+                  borderStyle: "solid",
+                  borderColor: "white",
+                  padding: "0 10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <p
+                  style={{
+                    textAlign: "center",
+                    color: "rgba(121.12, 121.12, 121.12, 1)",
+                    fontSize: 20}}
+                >
+                  {generatedUrl}
+                </p>
+              </div>
             </CopyToClipboard>
             <CopyToClipboard
               text={generatedUrl}
@@ -99,9 +136,41 @@ const Form = ({ isAuthenticated }) => {
               Close
             </button>
           </div>
+
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100vw",
+              height: "100vh",
+              backgroundColor: "rgba(0, 0, 0, 0.24)",
+              backdropFilter: "blur(2px)",
+              zIndex: "100",
+            }}
+          />
+        </div>
       )}
     </section>
   );
 };
 
 export default Form;
+/*
+<div style={{width: 666, height: 362, position: 'relative',}}>
+    <div style={{width: 656, height: 354, left: 10, top: 8, position: 'absolute', backgroundColor: 'black', borderRadius: 29, position: 'relative',}}>
+        <p style={{width: 70, left: 541, top: 235, position: 'absolute', fontSize: 14, fontWeight: '300', lineHeight: '100%', textAlign: 'center',}}>copy to clipboard</p>
+        <div style={{width: 68.72, height: 67, left: 542, top: 166, position: 'absolute',}}>
+            <div style={{width: 68.72, height: 67, paddingLeft: 9, paddingRight: 9, paddingTop: 5, paddingBottom: 5, backgroundColor: 'white', borderRadius: 9, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center',}}>
+                <img style={{width: 51.54, height: 56.69, boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 8,}} src="https://via.placeholder.com/51.53845977783203x56.69230651855469"/>
+            </div>
+        </div>
+        <div style={{width: 493, height: 100, left: 10, top: 149, position: 'absolute', borderRadius: 28, borderStyle: 'solid', borderWidth: 4, borderStyle: 'solid', borderColor: 'white',}}/>
+        <p style={{left: 33, top: 177, position: 'absolute', fontSize: 36, fontWeight: '300', lineHeight: '100%', color: 'rgba(121.12, 121.12, 121.12, 1)',}}>www.google.com/shorten</p>
+        <div style={{width: 32, height: 31, left: 579, top: 44, position: 'absolute', backgroundColor: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 8,}}/>
+        <p style={{width: 493, left: 10, top: 30, position: 'absolute', fontSize: 48, fontWeight: '600', lineHeight: '100%', color: 'white',}}>Short Url Generated!</p>
+    </div>
+    <div style={{width: 656, height: 352, left: 0, top: 0, position: 'absolute', backgroundColor: 'rgba(10, 25, 41, 1)', borderRadius: 29,}}/>
+</div>
+<div style={{width: 1920, height: 1080, backgroundColor: 'rgba(0, 0, 0, 0.24)',}}/>
+*/
